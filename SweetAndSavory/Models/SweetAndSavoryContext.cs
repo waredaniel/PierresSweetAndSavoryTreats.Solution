5e52657c68335dd0bf8eb2a7ba35e15fa6e79a76
danielware <waredanielb@gmail.com>
2022-08-12 08:45:02 -0700

    Add models data.

---
+++ SweetAndSavory/Models/SweetAndSavoryContext.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SweetAndSavory.Models
+{
+  public class SweetAndSavoryContext : IdentityDbContext<ApplicationUser>
+  {
+    public DbSet<Treat> Treats { get; set; }
+    public DbSet<Flavor> Flavors { get; set; }
+    public DbSet<FlavorTreat> FlavorTreats { get; set; }
+
+    public SweetAndSavoryContext(DbContextOptions options) : base(options) { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseLazyLoadingProxies();
+    }
+  }
+}
